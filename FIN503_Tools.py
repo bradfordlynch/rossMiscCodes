d7c44ebfc3df7e28da2df918823e353925d174f6
Bradford Lynch <blynch41@gmail.com>
2015-12-16 15:21:10 -0500

    Created tools for FIN503 class

---
+++ FIN503_Tools.py
@@ -0,0 +1,29 @@
+################################################################################
+
+#Tools for FIN503
+
+#Bradford Lynch, 2015, Ann Arbor, MI
+
+################################################################################
+
+import numpy as np
+
+class Bond(object):
+    def __init__(self, parValue, couponRate, maturity, currentYield, yieldToMaturity, price):
+        '''
+        Creates a bond security, potentially with some missing data.
+        '''
+        self.par = parValue
+        self.cr = couponRate
+        self.mat = maturity
+        self.cy = currentYield
+        self.ytm = yieldToMaturity
+        self.pr = price
+        
+    def calcPrice(self):
+        if self.par != None and self.cr != None and self.cy != None:
+            self.pr = (self.par*self.cr)/self.cy
+        elif self.par != None and self.cr != None and self.ytm != None:
+            self.pr = -1*np.pv(0.5*self.ytm, 2.0*self.mat, 0.5*self.par*self.cr, self.par)
+            
+        print "Bond price = $%.2f" % self.pr
